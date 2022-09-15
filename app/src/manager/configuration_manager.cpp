0f585f6d092e25420910d22fce51149b3ba2fce1
nitramr <martinreininger@gmx.net>
2022-09-15 19:02:08 +0200

    new lighter and darker theme

--- app/src/manager/configuration_manager.cpp
+++ app/src/manager/configuration_manager.cpp
@@ -13,7 +13,7 @@ QString ConfigurationManager::files(QString fileName)
 {
     QMap<QString, QString> *list = new QMap<QString, QString>();
     list->insert("app_layout", DirPathsManager::instance().settingsDir() + "layout_settings.ini");
-    list->insert("app_default_theme", DirPathsManager::instance().themeDir() + "scribus_light.qss");
+    list->insert("app_default_theme", DirPathsManager::instance().themeDir() + "scribus_dark.qss");
 
 
     return list->value(fileName, "");
